530dc290c5635f6744122996829b2b6419f08355
Chris-xcc <chris_guoc@163.com>
2020-06-10 19:37:38 +0800

    add history

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,5 +1,48 @@
 from django.contrib import admin
-from .models import GoodsType
+from django.core.cache import cache
+from .models import GoodsType, IndexPromotionBanner, IndexTypeGoodsBanner, IndexGoodsBanner
 
-# Register your models here.
-admin.site.register(GoodsType)
+
+class BaseModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        # 新增或者更新表中的数据时调用
+        super().save_model(request, obj, form, change)
+
+        # 发出任务让celery worker重新生成首页静态页
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清楚首页的缓存数据
+        cache.delete('index_page_data')
+
+    def delete_model(self, request, obj):
+        # 删除表中的数据时调用
+        super().delete_model(request, obj)
+        # 发出任务让celery worker重新生成首页静态页
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清楚首页的缓存数据
+        cache.delete('index_page_data')
+
+
+class GoodsTypeAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+    pass
+
+
+admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from .views import IndexView, DetailView
 
 app_name = 'goods'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),  # 首页
+    path('index/', IndexView.as_view(), name='index'),  # 首页
+    path('', IndexView.as_view(), name='index'),  # 首页
+    # re_path(r'goods/(?P<goods_id>\d+)/', DetailView.as_view(), name='detail'),  # 详情页
+    path('goods/<goods_id>/', DetailView.as_view(), name='detail'),  # 详情页
 ]
