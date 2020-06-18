7fcfcd030ea635d1f58b28d7f5c142bda646fc97
Chris-xcc <chris_guoc@163.com>
2020-06-18 10:01:55 +0800

    online

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#%wqcfr3z3n$3tfm+zy=i7c-alnpesbrg23upj*ca=)bz^wfe2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
